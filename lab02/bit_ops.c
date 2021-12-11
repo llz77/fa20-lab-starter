c6d63f1dd06652187e1ecb4ca053204398208144
llz77 <heisenberg9012@gmail.com>
2021-12-11 22:25:02 +0800

    lab2 finished

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,19 +9,30 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    // 右移动n位，然后与1取AND
+    x = x >> n;
+    return (x & 1);
 }
+
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    // 由于只改变第n位，所以x不移动
+    *x &= ~(1 << n);
+    *x |= v << n;
 }
+
+
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    // 单个1去做移位就正确了
+    unsigned y = 1 << n;
+    *x = *x ^ y;
 }
 
--- lab02/vector.c
+++ lab02/vector.c
@@ -21,11 +21,13 @@ static void allocation_failed() {
 /* Bad example of how to create a new vector */
 vector_t *bad_vector_new() {
     /* Create the vector and a pointer to it */
+    // retval和v指向同一个，重复了
     vector_t *retval, v;
     retval = &v;
 
     /* Initialize attributes */
     retval->size = 1;
+    // malloc需要做类型转换，并且也不是Int类型大小的地址，应该是struct类型大小
     retval->data = malloc(sizeof(int));
     if (retval->data == NULL) {
         allocation_failed();
@@ -38,6 +40,7 @@ vector_t *bad_vector_new() {
 /* Another suboptimal way of creating a vector */
 vector_t also_bad_vector_new() {
     /* Create the vector */
+    // 没有给结构体分配内存
     vector_t v;
 
     /* Initialize attributes */
@@ -58,27 +61,27 @@ vector_t *vector_new() {
     vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    retval = /* YOUR CODE HERE */
+    retval = (vector_t *) malloc(sizeof(vector_t));
 
     /* Check our return value to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval == NULL) {
         allocation_failed();
     }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    retval->size = /* YOUR CODE HERE */;
-    retval->data = /* YOUR CODE HERE */;
+    retval->size = 1;
+    retval->data = malloc(sizeof(int));
 
     /* Check the data attribute of our vector to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval->data == NULL) {
         free(retval);				//Why is this line necessary?
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
-    /* YOUR CODE HERE */ = 0;
+    retval->data[0] = 0;
 
     /* and return... */
     return retval;
@@ -96,8 +99,8 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    if (loc < /* YOUR CODE HERE */) {
-        return /* YOUR CODE HERE */;
+    if (loc < v->size) {
+        return *(v->data +loc);
     } else {
         return 0;
     }
@@ -107,6 +110,11 @@ int vector_get(vector_t *v, size_t loc) {
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
     /* YOUR SOLUTION HERE */
+    if (v == NULL) {
+        return;
+    }
+    free(v->data);
+    free(v);
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
@@ -117,4 +125,25 @@ void vector_set(vector_t *v, size_t loc, int value) {
      */
 
     /* YOUR SOLUTION HERE */
+    if (loc > (v->size - 1)) {
+        int prevSize = v->size;
+        int newSize = loc + 1;
+        int *newData = malloc(sizeof(int) * newSize);
+
+        if (newData == NULL) {
+            allocation_failed();
+        }
+        int p = 0;
+        // 复制内容
+        for (; p < prevSize; p++) {
+            newData[p] = v -> data[p];
+        }
+        for (; p < newSize; p++) {
+            newData[p] = 0;
+        }
+        free(v->data);
+        v->size = newSize;
+        v->data = newData;      
+    }
+    v->data[loc] = value;
 }
